1fb4a0a21912848a16cec7a614791905f43097f5
vaskovasilev <vasil.vasilev@midleo.com>
2021-10-11 20:24:27 +0200

    changed user history page

--- www/controller/modules/pages/browse/user.php
+++ www/controller/modules/pages/browse/user.php
@@ -23,7 +23,9 @@ if(!empty($tmp["user"]) && is_array($tmp["user"])){ ?>
 <div class="row">
     <div class="col-lg-4 col-md-12">
         <div class="card">
-            <div class="card-header"><h4>Profile</h4></div>
+            <div class="card-header">
+                <h4>Profile</h4>
+            </div>
             <div class="card-body">
                 <div class="mt-4 text-center">
                     <img src="<?php echo !empty($tmp["user"]["avatar"])?$tmp["user"]["avatar"]:"/assets/images/avatar.svg";?>"
@@ -50,7 +52,7 @@ if(!empty($tmp["user"]) && is_array($tmp["user"])){ ?>
                     }
                   }
                   ?>
-                
+
 
             </div>
 
@@ -58,34 +60,40 @@ if(!empty($tmp["user"]) && is_array($tmp["user"])){ ?>
     </div>
 
     <div class="col-lg-8 col-md-12">
-        <div class="card">
-            <div class="card-body">
 
-<?php $sql="select * from tracking where whoid=? order by id desc limit 20";
+        <?php $sql="select * from tracking where whoid=? order by id desc limit 20";
 $q = $pdo->prepare($sql);  
 $q->execute(array($thisarray['p2'])); 
 if($zobj = $q->fetchAll()){
 foreach($zobj as $val) { 
 
 ?>
-                <div class="sl-item mt-2 mb-3">
-                    <div class="sl-left">
-                        <div>
-                            <div class="d-flex">
-                                <h5 class="mb-0 font-weight-light"><a href="#"
-                                        class="link"><?php echo $tmp["user"]["fullname"];?></a></h5>
-                                <span class="sl-date text-muted ml-1"><?php echo textClass::ago($val["trackdate"]);?></span>
-                            </div>
-                            <p class="mt-2"> <?php echo $val["projid"];?><br> <?php echo $val["what"];?></p>
-                        </div>
+
+        <div class="card  p-2 mb-3">
+            <div class="d-flex align-items-start">
+                <img class="me-2 avatar-sm rounded-circle" src="/assets/images/avatar.svg"
+                    alt="<?php echo $tmp["user"]["fullname"];?>" style="width:40px;">
+                <div class="w-100">
+                    <h5 class="m-0"><?php echo $tmp["user"]["fullname"];?></h5>
+                    <p class="text-muted"><small><i
+                                class="mdi mdi-clock-outline"></i>&nbsp;<?php echo textClass::ago($val["trackdate"]);?></small>
+                    </p>
+
+                    <div>
+                        <p>
+                            <?php echo !empty($val["appid"])?'<a href="/env/apps/test'.$val["appid"].'" target="_blank"><i class="mdi mdi-application-brackets-outline"></i>&nbsp;'.$val["appid"]."</a><br>":"";?>
+                            <?php echo !empty($val["projid"])?'<i class="mdi mdi-projector-screen-outline"></i>&nbsp;'.$val["projid"]."<br>":"";?>
+                            <i class="mdi mdi-information-outline"></i>&nbsp;<?php echo $val["what"];?></p>
                     </div>
+
                 </div>
-                <hr>
-<?php } } else { echo "No activity yet."; } ?>
-
-
             </div>
+
         </div>
+
+        <?php } } else { echo "No activity yet."; } ?>
+
+
     </div>
 </div>
 
