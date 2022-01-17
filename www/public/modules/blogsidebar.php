d0ab0d533840025f07fa9f2a76cc39bbedb2ea93
Vasilev <vasil@vasilev.link>
2022-01-17 21:32:30 +0100

    fixed list item blogsidebar

--- www/public/modules/blogsidebar.php
+++ www/public/modules/blogsidebar.php
@@ -8,25 +8,32 @@
                     class="mdi mdi-plus"></i>&nbsp;New Page</a>
   </li>
             <?php if($forumcase=="posts"){?>
+              <li class="list-item">
             <a data-bs-toggle="tooltip" href="javascript:void(0)"
                 onclick="document.getElementById('pdfexport').click();" data-bs-placement="top" title="Export in PDF"
                 class="list-link"><i
                     class="mdi mdi-file-pdf-box"></i>&nbsp;Export in PDF</a>
+            </li>
+            <li class="list-item">
             <a data-bs-toggle="tooltip" href="/cpinfo/edit/<?php echo $keyws;?>" data-bs-placement="top"
                 title="Edit this Article"
                 class="list-link"><i
                     class="mdi mdi-pencil-outline"></i>&nbsp;Edit</a>
+            </li>
+            <li class="list-item">
             <a href="javascript:void(0)" onclick="getGITHistory('knowledge_info:<?php echo $zobj['id']; ?>');"
                 class="list-link"><i
                     class="mdi mdi-git"></i>&nbsp;Change History</a>
-
+            </li>
             <?php } ?>
             <?php if($breadcrumb){  if($brarr){ foreach($brarr as $key=>$val){ ?>
+              <li class="list-item">
             <a href="<?php echo $val["link"];?>" data-bs-toggle="tooltip" data-bs-placement="top"
                 title="<?php echo $val["title"];?>"
                 class="list-link <?php echo $val["main"]?"border-arrow":"";?>"><?php if($val["icon"]){?><i
                     class="mdi <?php echo $val["icon"];?>"></i><?php } ?><?php if($val["img"]){ ?><img
                     src="<?php echo $val["img"];?>" width="22px"></a><?php } ?>&nbsp;<?php echo $val["title"];?></a>
+                    </li>
             <?php }}} ?>
             <input type="submit" id="pdfexport" name="pdfexport" style="display:none;">
             <input type='hidden' name='thisid' value='<?php echo $keyws;?>'>
