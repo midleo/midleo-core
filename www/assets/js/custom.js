d5de3a57072c325c69a096f209cdf41d38d4904e
Vasilev <11207975+vvasil3v@users.noreply.github.com>
2022-07-17 18:59:44 +0200

    added getRandomStr

--- www/assets/js/custom.js
+++ www/assets/js/custom.js
@@ -564,3 +564,12 @@ function SetToString(set, delim){
   });
   return str
 }
+function getRandomStr(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+ }
+ return result;
+}
